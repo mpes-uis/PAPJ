9463be03daaf197f666151247376cdd78b13ef5c
Pedro Pelacani Berger <98188778+pedropberger@users.noreply.github.com>
2023-03-30 12:58:26 -0300

    Add files via upload

---
+++ email.R
@@ -0,0 +1,14 @@
+library("devtools")
+library(sendmailR)
+
+
+
+#### Envia o e-mail com authentication and SSL
+sendmail(from="pberger@mpes.mp.br", 
+         to=c("pedropberger@gmail.com"), 
+         subject="Teste final", 
+         msg=list(mime_part("Testando"), mime_part(filename_pdf), mime_part(filename_word)), 
+         engine = "curl", 
+         engineopts = list(username = username, password = password), 
+         control=list(smtpServer="smtp://smtp.office365.com:587", verbose = TRUE) 
+)
