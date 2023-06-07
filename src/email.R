60a4e33f99a5dfc6754adbf0ebf2248e7d0ed49c
Pedrow <pedropberger@gmail.com>
2023-06-07 15:28:15 -0300

    fix: e-mails enviados agora de age@mpes.mp.br

--- src/email.R
+++ src/email.R
@@ -10,7 +10,7 @@ texto <- paste("Exmo(a) Dr(a) ", nome, ", ", "segue em anexo formulário do Plan
 cat("Processando linha", nrow(df), "|", "email:", email)
 
 #### Envia o e-mail com authentication and SSL
-sendmail(from="pberger@mpes.mp.br", 
+sendmail(from="age@mpes.mp.br", 
          to=c(email), 
          subject="Formulário do Plano de Atuação das Promotorias de Justiça", 
          msg=list(mime_part(texto), mime_part(filename_pdf), mime_part(filename_word)), 
